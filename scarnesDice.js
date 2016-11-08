0e548c4970196e4e6662c6d85d2359686d8a0e1e
Ajay Verma <ajay_verma@ajay.av-Compaq-Presario-C700-Notebook-PC>
2016-11-08 19:43:20 +0530

    full working.

--- scarnesDice.js
+++ scarnesDice.js
@@ -3,11 +3,21 @@ var images = [];
 for(i=1;i<=6;i++){
     images.push("dice"+i+".png")
 };
+
 var userTotalScore = 0;
 var userTurnScore = 0;
 var compTotalScore = 0;
 var compTurnScore = 0;
 var diceVal = 0;
+var message = {
+    "user" : "YOUR TURN",
+    "comp" : "COMPUTER's TURN"
+};
+var changeMessage = function(text){
+    var messageID = $('#message');
+    messageID.empty();
+    messageID.html(text);
+}
 
 var userRoll = function(){
     randImgNo = Math.floor(Math.random()*5);
@@ -17,6 +27,7 @@ var userRoll = function(){
     if(diceVal != 1)userTurnScore += diceVal;
     else if (diceVal == 1){
         userTurnScore = 0;
+        changeMessage(message.comp);
         compChance();
     }
 }
@@ -24,37 +35,41 @@ var userHold = function () {
     userTotalScore += userTurnScore;
     userTurnScore = 0;
     updateScore();
+    changeMessage(message.comp);
     compChance();
 }
 var timer;
 var endCompChance = function () {
     clearInterval(timer);
+    changeMessage(message.user);
+    $('#roll').removeAttr('disabled');
+    $('#hold').removeAttr('disabled');
 };
 
 var compChance = function(){
-    console.log("in comp chance");
-
+    $('#roll').attr('disabled',true);
+    $('#hold').attr('disabled',true);
     timer = setInterval(function(){
         randImgNo = Math.floor(Math.random()*5);
         diceVal = randImgNo + 1;
-        console.log("setInterval");
         $('#aImage').attr('src',path+images[randImgNo]);
-        console.log("diceVal "+diceVal);
         if(diceVal == 1){
-            console.log("if diceVal");
             updateScore();
             endCompChance();
         }
         else {compTotalScore += diceVal;}
-    },3000);
+    },2000);
 };
 
 var updateScore = function () {
-    $('#userScore').empty();
-    $('#userScore').append(userTotalScore);
-    $('#compScore').empty();
-    $('#compScore').append(compTotalScore);
+    $('#userScore p').empty();
+    $('#userScore p').append(userTotalScore);
+    $('#compScore p').empty();
+    $('#compScore p').append(compTotalScore);
 };
 
 $('#roll').click(userRoll);
 $('#hold').click(userHold);
+$('#reset').click(function () {
+    location.reload();
+});
